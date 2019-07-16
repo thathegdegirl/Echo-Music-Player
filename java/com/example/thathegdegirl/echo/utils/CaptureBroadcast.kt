3068fec200199958b8c05d0c04a15d00530fdc90
Supriya Hegde <supriyahegdeofficial@gmail.com>
2019-07-16 22:47:06 +0530

    Add files via upload

---
+++ java/com/example/thathegdegirl/echo/utils/CaptureBroadcast.kt
@@ -0,0 +1,53 @@
+Supriya Hegdepackage com.example.thathegdegirl.echo.utils
+
+import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.telephony.TelephonyManager
+import com.example.thathegdegirl.echo.R
+import com.example.thathegdegirl.echo.activities.MainActivity
+import com.example.thathegdegirl.echo.fragments.SongPlayingFragment
+
+/**
+ * Created by Supriya Hegde.
+ */
+class CaptureBroadcast: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        if (p1?.action == Intent.ACTION_NEW_OUTGOING_CALL) {
+            try {
+                MainActivity.Statified.notificationManager?.cancel(345)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            try {
+                if (SongPlayingFragment.Statified.mediaplayer?.isPlaying as Boolean) {
+                    SongPlayingFragment.Statified.mediaplayer?.pause()
+                    SongPlayingFragment.Statified.playPauseImageButton?.setBackgroundResource(R.drawable.play_icon)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        } else {
+            val tm: TelephonyManager = p0?.getSystemService(Service.TELEPHONY_SERVICE) as TelephonyManager
+            when (tm?.callState) {
+                TelephonyManager.CALL_STATE_RINGING -> {
+                    try {
+                        MainActivity.Statified.notificationManager?.cancel(345)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                    try {
+                        if (SongPlayingFragment.Statified.mediaplayer?.isPlaying as Boolean) {
+                            SongPlayingFragment.Statified.mediaplayer?.pause()
+                            SongPlayingFragment.Statified.playPauseImageButton?.setBackgroundResource(R.drawable.play_icon)
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
+
+}
